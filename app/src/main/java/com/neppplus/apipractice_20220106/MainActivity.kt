eb04eb2ddfc1b315ca8c7d2a44e8cc31e40037fd
nepp-plus <nepp.plus@gmail.com>
2022-01-06 10:44:36 +0900

    레트로핏 / API기능명세 결합  - 그 결과물로, 로그인 기능 호출

--- app/src/main/java/com/neppplus/apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.apipractice_20220106.api.APIList
 import com.neppplus.apipractice_20220106.api.ServerAPI
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,9 +17,13 @@ class MainActivity : AppCompatActivity() {
             val inputEmail = edtEmail.text.toString()
             val inputPw = edtPassword.text.toString()
 
-//            서버 API의 로그인 기능에 활용.
+//            서버 API의 로그인 기능에 활용. (ServerAPI 클래스 / APIList 인터페이스 결합)
 
+            val retrofit = ServerAPI.getRetrofit() // API 연결 도구 생성
+            val apiList = retrofit.create(APIList::class.java) // 연결도구 + 기능목록 결합 객체 생성.
 
+//            실제 로그인 기능 호출 (Request)
+            apiList.postRequestLogin(inputEmail, inputPw)
 
 
         }
