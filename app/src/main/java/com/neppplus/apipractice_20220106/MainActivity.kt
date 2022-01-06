5c3ee609b7fbc19f6d0612ab3f2a7e8be874d340
nepp-plus <nepp.plus@gmail.com>
2022-01-06 09:54:00 +0900

    레트로핏 객체 생성 기능을 가지게 될 클래스 ServerAPI 클래스 제작중

--- app/src/main/java/com/neppplus/apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/MainActivity.kt
@@ -2,10 +2,13 @@ package com.neppplus.apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.apipractice_20220106.api.ServerAPI
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
---
+++ app/src/main/java/com/neppplus/apipractice_20220106/api/ServerAPI.kt
@@ -0,0 +1,16 @@
+package com.neppplus.apipractice_20220106.api
+
+class ServerAPI {
+    
+//    서버통신을 하게 도와주는 클래스 (Retrofit) 를 객체로 만들어주는 기능.
+    
+//    함수 한번만 호출 하면 => 편하게 가져다 쓸 수 있게 함수로 만들어두자.
+    
+//    클래스이름.기능() => @classmethod 에 대응되는 기능 활용.
+    
+    companion object {
+        
+
+    }
+    
+}
