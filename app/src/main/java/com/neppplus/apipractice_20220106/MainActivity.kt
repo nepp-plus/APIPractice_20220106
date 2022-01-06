d7497235d90db716d6de5381378f233fb58a45a0
nepp-plus <nepp.plus@gmail.com>
2022-01-06 16:14:53 +0900

    서버에 다녀오는 행위 : 오래 걸리는 동작.  - 리싸이클러뷰 세팅보다 오래 걸림.  - 코드는 먼저 실행되어도 => 데이터 추가는 나중에 될 수도 있다.  - notifyData... 으로 변경사항 반영

--- app/src/main/java/com/neppplus/apipractice_20220106/MainActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/MainActivity.kt
@@ -32,6 +32,10 @@ class MainActivity : BaseActivity() {
 
                     mReviewList.addAll( br.data.reviews )
 
+//                    리싸이클러뷰 세팅 완료 이후에 => 데이터를 추가한다면, 자동 반영 X.
+//                    어댑터의 내용 변경 -> 반영 해달라. (어댑터에게 요청)
+                    mReviewAdapter.notifyDataSetChanged()
+
                 }
 
             }
