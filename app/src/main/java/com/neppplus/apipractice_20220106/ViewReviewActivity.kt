79eb59f9c58e543508da482abbf1cc2abc307ac2
nepp-plus <nepp.plus@gmail.com>
2022-01-07 15:08:41 +0900

    리뷰 상세보기 API 호출  - Path Segment를 Retrofit으로 활용하는 방법

--- app/src/main/java/com/neppplus/apipractice_20220106/ViewReviewActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/ViewReviewActivity.kt
@@ -2,10 +2,29 @@ package com.neppplus.apipractice_20220106
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.apipractice_20220106.models.BasicResponse
+import com.neppplus.apipractice_20220106.models.ReviewData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ViewReviewActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_review)
+
+        val reviewData = intent.getSerializableExtra("review") as ReviewData
+
+        apiList.getRequestReviewDetail(reviewData.id).enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+
+        })
+
     }
 }
