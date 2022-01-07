a1abcc460295cab03afbc7c414af1a458b453245
nepp-plus <nepp.plus@gmail.com>
2022-01-07 15:17:03 +0900

    제목 / 내용 서버에서 받아온 새 데이터로 반영.

--- app/src/main/java/com/neppplus/apipractice_20220106/ViewReviewActivity.kt
+++ app/src/main/java/com/neppplus/apipractice_20220106/ViewReviewActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.apipractice_20220106.models.BasicResponse
 import com.neppplus.apipractice_20220106.models.ReviewData
+import kotlinx.android.synthetic.main.activity_view_review.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,6 +18,14 @@ class ViewReviewActivity : BaseActivity() {
 
         apiList.getRequestReviewDetail(reviewData.id).enqueue(object : Callback<BasicResponse> {
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                val br = response.body()!!
+
+                val newReviewData = br.data.review
+
+                txtReviewTitle.text = newReviewData.title
+                txtReviewContent.text = newReviewData.content
+
 
             }
 
